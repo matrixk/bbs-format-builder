6574c54e61951d862b26fce2c55c68b05959ffcb
古川政良 <matrixk@outlook.com>
2022-03-09 20:15:41 +0800

    Update forum_format.py

--- forum_format.py
+++ forum_format.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Nov 25 11:01:32 2017
-
+Update: 2022-03-09 20:15:31
 @author: matrixk
 """
 import random
@@ -10,26 +10,26 @@ import random
 f = open('forum_format.md', 'w')
 
 def fluctuation(hour_flu):
-    standard = 60
-    flufluc = [2.73, 1.66, 1.10, 0.80, 0.66, 0.65, 0.91, 1.64, 3.32, 4.81, 5.56, 5.64, 5.53, 5.57, 5.83, 6.00, 6.13, 5.81, 5.68, 6.39, 6.79, 6.59, 5.77, 4.25]
+    standard = 30
+    flufluc = [2.73, 1.66, 1.10, 0.60, 0.14, 0.05, 0.24, 1.64, 3.32, 4.81, 5.56, 5.64, 5.53, 5.57, 5.83, 6.00, 6.13, 5.81, 5.68, 6.39, 6.79, 6.59, 5.77, 4.25]
     standard = standard / flufluc[int(hour_flu)]
     return standard
 
 def forum_format(date, hour_start, min_start, sec_start, floor):
     #set initial
-    floor_number = 25
+    floor_number = 0
     date_show = date
     time_hour = int(hour_start)
     time_min = int(min_start)
     time_sec = int(sec_start)
-    standard = 60
+    standard = 30
     
     for i in range(floor_number,int(floor)+1):
         standard = fluctuation(time_hour)
         f.write (u"\u2116".encode("utf-8")+str(floor_number)+u"\u0020\u2606\u2606\u2606=\u0020=于".encode("utf-8"))
         f.write (str(date_show) + u"\u0020".encode("utf-8"))
         f.write (str(time_hour).zfill(2) + ":" + str(time_min).zfill(2) + ":" + str(time_sec).zfill(2) +u"留言\u2606\u2606\u2606".encode("utf-8"))
-        f.write ("\n\n- - - - -\n\n\n\n")
+        f.write ("\n\n----------\n\n\n\n")
         floor_number = floor_number + 1
         time_sec = time_sec + random.randint(1,59)
         if time_sec >= 60:
